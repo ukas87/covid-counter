71918b5dcd244befd93acd2f26e59688fcfc0e19
Łukasz Lewandowski <lukastonie@gmail.com>
2022-04-15 23:13:05 +0200

    adding html(thymeleaf) and lombok

--- src/main/java/io/javaexample/coronavirustracker/models/LocationStatistics.java
+++ src/main/java/io/javaexample/coronavirustracker/models/LocationStatistics.java
@@ -1,43 +1,19 @@
 package io.javaexample.coronavirustracker.models;
 
-public class LocationStatistics {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
+public class LocationStatistics {
 
 
     private String state;
     private String country;
     private int latestTotalCase;
 
-    public String getState() {
-        return state;
-    }
 
-    public void setState(String state) {
-        this.state = state;
-    }
 
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public int getLatestTotalCase() {
-        return latestTotalCase;
-    }
-
-    public void setLatestTotalCase(int latestTotalCase) {
-        this.latestTotalCase = latestTotalCase;
-    }
-
-    @Override
-    public String toString() {
-        return "LocationStatistics{" +
-                "state='" + state + '\'' +
-                ", country='" + country + '\'' +
-                ", latestTotalCase=" + latestTotalCase +
-                '}';
-    }
 }
